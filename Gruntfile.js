f35d15575ae54f67163180e9a89afaaebb40f012
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-05-03 04:21:09 +0000

    build: Updating eslint-config-wikimedia to 0.29.1
    Additional changes:
    * eslint: Replaced `wikimedia/client-es5` with `wikimedia/client`.
    
    Change-Id: Ieabb45b2167ed0a2f1d9555ca5d2b746b28b023d

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /* eslint-env node, es6 */
 module.exports = function ( grunt ) {
-	var conf = grunt.file.readJSON( 'extension.json' );
+	const conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
@@ -12,7 +12,7 @@ module.exports = function ( grunt ) {
 			},
 			all: '.'
 		},
-		// eslint-disable-next-line es-x/no-object-assign
+
 		banana: Object.assign(
 			conf.MessagesDirs,
 			{
